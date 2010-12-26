fb9e9a2bf4a5872615f9cf1a908b49713ae1877c
babyblue <babyblue@192.168.1.198>
2010-12-26 23:37:14 +0100

    variabel tillagd

--- src/emotiometer/control/Constant.java
+++ src/emotiometer/control/Constant.java
@@ -1,9 +1,18 @@
 package emotiometer.control;
 public class Constant 
 {
-	static String[] Positive = { ":)", ":-)", ":]", "=)", ":-D", ":D", "=D", ";-)", ";)", ":-P", ":P", ":-p", ":p", "=P", };
+	int RESULTAT_NOTHING = 0;
+	//om tweetet inte innehåller någon emoticon//
+	int RESULTAT_POSITIVE = 1;
+	//den innehåller en positiv//
+	int RESULTAT_NEGATIVE = 2;
+	//den innehåller en negativ//
+	int RESULTAT_BOTH = 3;
+	//den innehåller båda//
+	
+	static String[] Positive = { ":)", ":-)", ":]", "=)", ":-D", ":D", "=D", ";-)", ";)", ":-P", ":P", ":-p", ":p", "=P", "=>", ":>", ":->", "8-)", "8)", "8D", "8-D", "8-)", };
 	//arrayPositive,smile,grin,wink,tongue//
-	static String[] Negative = { ":-(", ":(", ":[", "=(", ">:(", ">:-(", ">:O", ">:-O", ">:o", ">:-o", };
+	static String[] Negative = { ":-(", ":(", ":[", "=(", ">:(", ">:-(", ">:O", ">:-O", ">:o", ">:-o", "=<", ":<", ":-<", "8-(", "8-/", "8/", "=/", ":/", ":-/", };
 	//arrayNegative,frown,grumpy,upset// 
 	{
 		for (int i=0; i<Positive.length; i++){
