7c424302c9e9e5b72ac1a1e5290c595ef3132f23
babyblue <babyblue@192.168.1.198>
2010-12-26 15:11:52 +0100

    lagt till array med smile och frown

--- src/emotiometer/control/Constant.java
+++ src/emotiometer/control/Constant.java
@@ -1,10 +1,19 @@
 package emotiometer.control;
-
-public class Constant {
-	
-	public String prodName1 = "Tron";
-	public String prodName2 = "TRON";
-	public String prodName3 = "tron";
-	
-
+public class Constant 
+{
+	static String[] Positive = { ":)", ":-)", ":]", "=)", ":-D", ":D", "=D", ";-)", ";)", ":-P", ":P", ":-p", ":p", "=P", };
+	//arrayPositive,smile,grin,wink,tongue//
+	static String[] Negative = { ":-(", ":(", ":[", "=(", ">:(", ">:-(", ">:O", ">:-O", ">:o", ">:-o", };
+	//arrayNegative,frown,grumpy,upset// 
+	{
+		for (int i=0; i<Positive.length; i++){
+		}//reads Positive String Array//
+		for (int i=0; i<Negative.length; i++){
+		}//reads Negative String Array//	
+	}
+	public String prodName1 = " Tron ";
+	public String prodName2 = " TRON ";
+	public String prodName3 = " tron ";
 }
+
+
