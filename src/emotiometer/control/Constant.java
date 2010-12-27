c81645cf4aade3b7c0294f8019f1f669f591a37d
Larmen <anders.larm@gmail.com>
2010-12-27 09:58:38 +0100

    Changed value of String Prodnames in Constant class

--- src/emotiometer/control/Constant.java
+++ src/emotiometer/control/Constant.java
@@ -1,6 +1,11 @@
 package emotiometer.control;
 public class Constant 
 {
+	public String prodName1 = "Tron Legacy";
+	public String prodName2 = "TRON LEGACY";
+	public String prodName3 = "tron legacy";
+	
+	
 	final int RESULTAT_NOTHING = 0;
 	//om tweetet inte innehåller någon emoticon//
 	final int RESULTAT_POSITIVE = 1;
@@ -20,9 +25,7 @@ public class Constant
 		for (int i=0; i<Negative.length; i++){
 		}//reads Negative String Array//	
 	}
-	public String prodName1 = " Tron ";
-	public String prodName2 = " TRON ";
-	public String prodName3 = " tron ";
+
 }
 
 
